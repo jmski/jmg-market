999515d5301e6d37493d1fb81449a366f61cfe80
jmski <geluajm@gmail.com>
2023-03-11 23:36:42 -0500

    env local whoopies

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
           const newImage = img
             .replace(
               "image-",
-              "https://cdn.sanity.io/images/vfxfwnaw/production/"
+              "https://cdn.sanity.io/images/o2oyq743/production/"
             )
             .replace("-webp", ".webp");
 
