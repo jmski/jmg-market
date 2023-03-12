38d7cb7562cab787a2de73196af6dec19db40a3e
jmski <geluajm@gmail.com>
2023-03-11 22:43:23 -0500

    stripe integration and components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+import React from "react";
 import "@/styles/globals.scss";
 import "@/styles/variables.module.scss";
 
+import { Layout } from "@/components";
+import { StateContext } from "@/context/StateContext";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
